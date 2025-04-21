fec0c42afc4610a560ee4a9a251f5b5ebb9d42eb
ycha613 <ycha613@aucklanduni.ac.nz>
2025-04-21 21:10:45 +1200

    added comment in init file with details on using api to get data

--- project/__init__.py
+++ project/__init__.py
@@ -12,3 +12,12 @@ def create_app():
         pass
 
     return app
+
+# create a dictionary of the 20 teams with keys as ids (teams_dict)
+# create a list of all players (test_players)
+# each team has a list of players, each player has a reference to its team
+
+# go through fixtures api, create a dictionary for each team
+# value of dict is list of fixture ids of results
+# also create TeamResult and TeamFixture objects
+# then for each player go through ["history"] mapping fixture ids to results
